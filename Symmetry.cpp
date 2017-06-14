cd9475f1f2353247d235ad77c27baa98e6fbbed5
Arkadiy Simonov <arkadiy.simonov@chem.ox.ac.uk>
2017-06-14 19:40:57 +0100

    Before added slicing

--- Symmetry.cpp
+++ Symmetry.cpp
@@ -24,6 +24,8 @@ vector<Matrix3i> expand_generators(const vector<Matrix3i>& generators) {
         }
     }
 
+    auto last = unique(res.begin(), res.end());
+    res.erase(last, res.end());
     return res;
 }
 
@@ -46,7 +48,8 @@ vector<Matrix3i> expand_symmetry(const string& symmetry) {
                                   el( x,-y, z),
                                   el( x, y,-z),
                                   el( y, z, x),
-                                  el( x, y,-z)});
+                                  el( y, x, z),
+                                  el(-y, x, z)});
     }else if(symmetry == "mmm") {
         return expand_generators({el(-x, y, z),
                                   el( x,-y, z),
@@ -65,12 +68,41 @@ inline size_t ind2ind(const Vector3i& r, const vector<size_t>& size,const vector
     return ((r[0]+centre[0])*size[1]+(r[1]+centre[1]))*size[2]+(r[2]+centre[2]);
 }
 
-
-float average_set_of_reflections(vector<double> &equivalent_intensities, const vector<double>::iterator &intensities_last, const InputParameters& par) {
+inline float mean(const vector<float>::iterator &start, const vector<float>::iterator &end) {
     float average = 0;
-    for_each(equivalent_intensities.begin(), intensities_last, [&](const float& i){average += i;});
-    average = average/distance(equivalent_intensities.begin(), intensities_last);
+    for_each(start,
+             end,
+             [&](const float& i){
+                 average += i;
+             });
+    average = average/distance(start, end);
     return average;
+}
+
+inline float median(const vector<float>::iterator &start, const vector<float>::iterator &end) {
+    sort(start, end);
+    auto sz = distance(start, end);
+    return *(start+sz/2);
+}
+
+inline const vector<float>::iterator reject_outliers(const vector<float>::iterator & start, const vector<float>::iterator & end, float threshold) {
+    auto sz = distance(start, end);
+    float med = median(start, end);
+
+    vector<float> differences(sz);
+    transform(start, end, differences.begin(), [&](const float& i) {return i - med;} );
+
+    float median_distance = median(differences.begin(), differences.end());
+
+    return remove_if(start, end, [&](const float& i){return abs(i - med)>median_distance*threshold;});
+}
+
+inline float average_set_of_reflections(const vector<float>::iterator &equivalent_intensities,  vector<float>::iterator &intensities_last, const InputParameters& par) {
+    if(par.reject_outliers && distance(equivalent_intensities, intensities_last)>2) {
+        intensities_last = reject_outliers(equivalent_intensities, intensities_last, par.threshold);
+    }
+
+    return mean(equivalent_intensities, intensities_last);
 }
 
 void average(IntensityData<float>& inp, IntensityData<float>& res, const InputParameters& par) {
@@ -87,7 +119,7 @@ void average(IntensityData<float>& inp, IntensityData<float>& res, const InputPa
     //TODO: rewrite this loop in a linear fashion??
     vector<Matrix3i> symmetry_elements = expand_symmetry(par.symmetry);
     vector<size_t> equivalent_indices(symmetry_elements.size());
-    vector<double> equivalent_intensities(symmetry_elements.size());
+    vector<float> equivalent_intensities(symmetry_elements.size());
     Vector3i r;
     for(r(0) = -centre[0]; r(0) < (long)(size[0])-centre[0]; ++r(0))
         for(r(1) = -centre[1]; r(1) < (long)(size[1])-centre[1]; ++r(1))
@@ -107,9 +139,9 @@ void average(IntensityData<float>& inp, IntensityData<float>& res, const InputPa
                                                       equivalent_intensities.begin(),
                                                       [&](const size_t& i){return inp.data[i];});
 
-                    intensities_last = remove_if(equivalent_intensities.begin(),intensities_last, isnan<float>);
+                    intensities_last = remove_if(equivalent_intensities.begin(), intensities_last, isnan<float>);
 
-                    float average = average_set_of_reflections(equivalent_intensities, intensities_last, par);
+                    float average = average_set_of_reflections(equivalent_intensities.begin(), intensities_last, par);
 
                     //put the average in correct places
                     for_each(equivalent_indices.begin(),
@@ -120,6 +152,26 @@ void average(IntensityData<float>& inp, IntensityData<float>& res, const InputPa
                               });
 
             }
+
+
+//    for(int i = 0; i < no_files; i += chunk_dims[2]) {
+//        int tile_i;
+//        for(tile_i = i; tile_i < min(i + static_cast<int>(chunk_dims[2]), static_cast<int>(no_files)); ++tile_i) {
+//            CBFDataReader frame(argv[2 + tile_i]);
+//            frame.read_data(buffer.data() + size[1]*size[2]*(tile_i - i));
+//        }
+//
+//        hsize_t noFramesInTile = tile_i - i;
+//        hsize_t memspaceOffset[] = {0, 0, 0};
+//        hsize_t spaceCount[] = {noFramesInTile, memspaceSize[1], memspaceSize[2]};
+//        memspace.selectHyperslab(H5S_SELECT_SET, spaceCount, memspaceOffset);
+//
+//        hsize_t fspaceOffset[] = {static_cast<hsize_t>(i), 0, 0};
+//        fspace.selectHyperslab(H5S_SELECT_SET, spaceCount, fspaceOffset);
+//
+//        dataset.write(buffer.data(), PredType::NATIVE_INT32, memspace, fspace);
+//    }
+
 
 }
 
